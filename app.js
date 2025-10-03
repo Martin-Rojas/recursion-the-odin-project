4bd7a1ef491222341f22bc048286cc84c31662b6
Martin-Rojas <rojas.martin25@yahoo.com>
2025-10-02 17:25:29 -0700

    code for function power(exponent, base)

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ function sumRange(n, totalSum = 0) {
   } else return n + sumRange(n - 1);
 }
 
-console.log(sumRange(3));
+//console.log(sumRange(3));
+
+function power(exponent, base) {
+  if (exponent == 0) {
+    return 1;
+  } else return base * power(exponent - 1, base);
+}
+
+console.log(power(4, 2));
